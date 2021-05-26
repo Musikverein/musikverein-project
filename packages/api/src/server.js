f580ce7298f9d3945a628960a7c21c5d38004fc9
Raúl Cátedra <raulcabe3003@gmail.com>
2021-05-26 13:53:23 +0200

    fix: try to change cors

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -25,6 +25,10 @@ app.use(json());
 app.use(
   cors({
     origin: config.client.url,
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    optionsSuccessStatus: 200,
+    preflightContinue: false,
   }),
 );
 
