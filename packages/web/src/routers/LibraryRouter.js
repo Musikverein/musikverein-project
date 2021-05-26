47cc7b7cf50670d47e37dbcfc10e852b5e3f27ba
Verónica Velázquez <veronicavelazquezcalleja@gmail.com>
2021-05-26 10:54:12 +0200

    fix: name of folder

--- packages/web/src/routers/LibraryRouter.js
+++ packages/web/src/routers/LibraryRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect, Switch } from 'react-router-dom';
 import ProtectedRoute from '../components/ProtectedRoute';
-import LibraryPlayLists from '../pages/LibraryPlayLists';
+import LibraryPlayLists from '../pages/LibraryPlaylists';
 import LibrarySongs from '../pages/LibrarySongs';
 import { authSelector } from '../redux/auth/auth-selectors';
 import ROUTES from './routes';
